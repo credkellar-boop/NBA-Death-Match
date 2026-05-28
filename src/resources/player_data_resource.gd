e095d81ce809cdf33a8dbfcec695f99fa240dcbb
Darion Kellar <credkellar@gmail.com>
2026-05-27 22:06:23 -0700

    Create src/resources/player_data_resource.gd

---
+++ src/resources/player_data_resource.gd
@@ -0,0 +1,6 @@
+# Update to your res://src/resources/player_data_resource.gd
+
+@export var player_name: String
+@export var speed: float
+@export var attack_power: float
+@export var signature_move_id: String # e.g., "JORDAN_DUNK", "SHAQ_SMASH"
